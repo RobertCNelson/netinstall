913a48ed64e8cf38405c7524fe17b7f73ad6cf52
Robert Nelson <robertcnelson@gmail.com>
2010-04-23 16:13:24 -0500

    noticed by noway on irc, yeah serial doesnt need omapfb settings..

--- scripts/serial-normal-squeeze.cmd
+++ scripts/serial-normal-squeeze.cmd
@@ -1,4 +1,4 @@
 setenv bootcmd 'mmc init; fatload mmc 0:1 0x80000000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80000000 0x81600000'
-setenv bootargs 'console=ttyS2,115200n8 root=/dev/mmcblk0p2 ro omapfb.mode=dvi:1280x720MR-16@60'
+setenv bootargs 'console=ttyS2,115200n8 root=/dev/mmcblk0p2 ro'
 boot
 
--- scripts/serial.cmd
+++ scripts/serial.cmd
@@ -1,4 +1,4 @@
 setenv bootcmd 'mmc init; fatload mmc 0:1 0x80000000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80000000 0x81600000'
-setenv bootargs 'console=ttyS2,115200n8 root=/dev/ram0 rw omapfb.mode=dvi:1280x720MR-16@60'
+setenv bootargs 'console=ttyS2,115200n8 root=/dev/ram0 rw'
 boot
 
