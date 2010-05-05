5f862b163622accbabac1e62a1d50af0f0d6de8f
Robert Nelson <robertcnelson@gmail.com>
2010-05-05 11:34:56 -0500

    pass uboot buddy variable for zippy1/2 detection

--- scripts/serial-normal-lucid.cmd
+++ scripts/serial-normal-lucid.cmd
@@ -1,4 +1,4 @@
 setenv bootcmd 'mmc init; fatload mmc 0:1 0x80000000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80000000 0x81600000'
-setenv bootargs 'console=ttyS2,115200n8 root=/dev/mmcblk0p5 ro fixrtc'
+setenv bootargs console=ttyS2,115200n8 root=/dev/mmcblk0p5 ro fixrtc buddy=${buddy}
 boot
 
