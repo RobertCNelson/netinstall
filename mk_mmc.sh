98a6032bdc9c657f6fb10b254f77534279e9e958
Robert Nelson <robertcnelson@gmail.com>
2010-08-23 09:43:36 -0500

    check_mmc fixes

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -281,15 +281,15 @@ echo "done"
 }
 
 function check_mmc {
- DISK_NAME="Disk|Platte"
- FDISK=$(sudo fdisk -l | grep "[${DISK_NAME}] ${MMC}" | awk '{print $2}')
+ DISK_NAME="Disk|Platte|Disco"
+ FDISK=$(sudo fdisk -l 2>/dev/null | grep "[${DISK_NAME}] ${MMC}" | awk '{print $2}')
 
  if test "-$FDISK-" = "-$MMC:-"
  then
   echo ""
   echo "I see..."
   echo "sudo fdisk -l:"
-  sudo fdisk -l | grep "[${DISK_NAME}] /dev/" --color=never
+  sudo fdisk -l 2>/dev/null | grep "[${DISK_NAME}] /dev/" --color=never
   echo ""
   echo "mount:"
   mount | grep -v none | grep "/dev/" --color=never
@@ -302,7 +302,7 @@ function check_mmc {
   echo "Are you sure? I Don't see [${MMC}], here is what I do see..."
   echo ""
   echo "sudo fdisk -l:"
-  sudo fdisk -l | grep "[${DISK_NAME}] /dev/" --color=never
+  sudo fdisk -l 2>/dev/null | grep "[${DISK_NAME}] /dev/" --color=never
   echo ""
   echo "mount:"
   mount | grep -v none | grep "/dev/" --color=never
