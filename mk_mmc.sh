a7b754bbae521667f3f25a8d53ad2946bd7016e4
Robert Nelson <robertcnelson@gmail.com>
2010-11-29 10:28:47 -0600

    use serial.cmd if available
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -437,6 +437,9 @@ sudo mount -o remount,rw /boot/uboot
 if ls /boot/uboot/boot.cmd >/dev/null 2>&1;then
 sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/boot.cmd /boot/uboot/boot.scr
 fi
+if ls /boot/uboot/serial.cmd >/dev/null 2>&1;then
+sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Boot Script" -d /boot/uboot/serial.cmd /boot/uboot/boot.scr
+fi
 sudo cp /boot/uboot/boot.scr /boot/uboot/boot.ini
 if ls /boot/uboot/user.cmd >/dev/null 2>&1;then
 sudo mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Reset Nand" -d /boot/uboot/user.cmd /boot/uboot/user.scr
