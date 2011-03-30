e2b366900910b20df31d71e305e2084f15dbbfcf
Robert Nelson <robertcnelson@gmail.com>
2011-03-30 10:58:12 -0500

    so close to not needing the 800Mhz limit anymore...
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/boot.scr/serial.cmd
+++ scripts/boot.scr/serial.cmd
@@ -7,6 +7,10 @@ if test "${beaglerev}" = "xMB"; then
 echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
 setenv mpurate 800
 fi
+if test "${beaglerev}" = "xMC"; then
+echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
+setenv mpurate 800
+fi
 setenv bootcmd 'mmc init; fatload mmc 0:1 0x80300000 uImage.net; fatload mmc 0:1 0x81600000 uInitrd.net; bootm 0x80300000 0x81600000'
 setenv bootargs console=ttyO2,115200n8 root=/dev/ram0 rw buddy=${buddy} mpurate=${mpurate}
 boot
