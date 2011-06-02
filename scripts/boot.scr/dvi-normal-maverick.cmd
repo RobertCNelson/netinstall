174fef0f746996a126f38a1559b90e090c8e52a7
Robert Nelson <robertcnelson@gmail.com>
2011-06-01 21:36:05 -0500

    cmd: the 2.6.39 kernel does a good job of selecting the correct cpufreq, so the 800mhz limiation isnt needed anymore
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/boot.scr/dvi-normal-maverick.cmd
+++ scripts/boot.scr/dvi-normal-maverick.cmd
@@ -1,16 +1,4 @@
 echo "Debug: Maverick DVI"
-if test "${beaglerev}" = "xMA"; then
-echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
-setenv mpurate 800
-fi
-if test "${beaglerev}" = "xMB"; then
-echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
-setenv mpurate 800
-fi
-if test "${beaglerev}" = "xMC"; then
-echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
-setenv mpurate 800
-fi
 setenv dvimode 1280x720MR-16@60
 setenv vram 12MB
 setenv bootcmd 'fatload mmc 0:1 0x80300000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80300000 0x81600000'
--- scripts/boot.scr/serial-normal-maverick.cmd
+++ scripts/boot.scr/serial-normal-maverick.cmd
@@ -1,16 +1,4 @@
 echo "Debug: Maverick Serial"
-if test "${beaglerev}" = "xMA"; then
-echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
-setenv mpurate 800
-fi
-if test "${beaglerev}" = "xMB"; then
-echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
-setenv mpurate 800
-fi
-if test "${beaglerev}" = "xMC"; then
-echo "Kernel is not ready for 1Ghz limiting to 800Mhz"
-setenv mpurate 800
-fi
 setenv bootcmd 'fatload mmc 0:1 0x80300000 uImage; fatload mmc 0:1 0x81600000 uInitrd; bootm 0x80300000 0x81600000'
 setenv bootargs console=ttyO2,115200n8 root=/dev/mmcblk0p5 ro fixrtc buddy=${buddy} mpurate=${mpurate}
 boot
