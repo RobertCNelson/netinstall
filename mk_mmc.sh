541498305f4b0ee54e82bcb568b4f80523916d57
Robert Nelson <robertcnelson@gmail.com>
2012-02-21 16:21:15 -0600

    setup_sdcard.sh: Add helper is_element_of() function.
    Add a simple helper function is_element_of() that will be used to
    simplify the validation of various values.  This routine is not meant
    to be called directly; instead, it will be used to write much simpler
    validation routines to check for acceptable values like a filesystem
    type and so on.
    
    Signed-off-by: Robert P. J. Day <rpjday@crashcourse.ca>
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -90,6 +90,14 @@ SQUEEZE_MD5SUM="f8d7e14b73c1cb89ff09c79a02694c22"
 DIR="$PWD"
 TEMPDIR=$(mktemp -d)
 
+function is_element_of {
+	testelt=$1
+	for validelt in $2 ; do
+		[ $testelt = $validelt ] && return 0
+	done
+	return 1
+}
+
 function check_root {
 if [[ $UID -ne 0 ]]; then
  echo "$0 must be run as sudo user or root"
