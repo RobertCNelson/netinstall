c7949332da7d087982e12b13d7cf87cd3276d39a
Robert Nelson <robertcnelson@gmail.com>
2012-03-13 12:24:42 -0500

    mk_mmc add KMS video= options
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -1550,18 +1550,31 @@ function check_mmc {
 }
 
 function is_omap {
- IS_OMAP=1
- SPL_BOOT=1
- UIMAGE_ADDR="0x80300000"
- UINITRD_ADDR="0x81600000"
- SERIAL_CONSOLE="${SERIAL},115200n8"
- ZRELADD="0x80008000"
- SUBARCH="omap"
- VIDEO_CONSOLE="console=tty0"
- VIDEO_DRV="omapfb.mode=dvi"
- VIDEO_OMAP_RAM="12MB"
- VIDEO_OMAPFB_MODE="dvi"
- VIDEO_TIMING="1280x720MR-16@60"
+	IS_OMAP=1
+	SPL_BOOT=1
+	SUBARCH="omap"
+
+	UIMAGE_ADDR="0x80300000"
+	UINITRD_ADDR="0x81600000"
+
+	ZRELADD="0x80008000"
+
+	SERIAL_CONSOLE="${SERIAL},115200n8"
+
+	VIDEO_CONSOLE="console=tty0"
+
+	#Older DSS2 omapfb framebuffer driver:
+	VIDEO_DRV="omapfb.mode=dvi"
+	VIDEO_OMAP_RAM="12MB"
+	VIDEO_OMAPFB_MODE="dvi"
+	VIDEO_TIMING="1280x720MR-16@60"
+
+	#KMS Video Options (overrides when edid fails)
+	# From: ls /sys/class/drm/
+	# Unknown-1 might be s-video..
+	KMS_VIDEO_RESOLUTION="1280x720"
+	KMS_VIDEOA="video=DVI-D-1"
+	KMS_VIDEOB=""
 }
 
 function is_imx53 {
@@ -1643,6 +1656,7 @@ function check_uboot_type {
 		USE_UENV=1
 		is_omap
 		VIDEO_OMAP_RAM="16MB"
+		KMS_VIDEOB="video=HDMI-A-1"
 		;;
 	panda_es)
 		SYSTEM="panda_es"
@@ -1653,6 +1667,7 @@ function check_uboot_type {
 		USE_UENV=1
 		is_omap
 		VIDEO_OMAP_RAM="16MB"
+		KMS_VIDEOB="video=HDMI-A-1"
 		;;
 	touchbook)
 		SYSTEM="touchbook"
@@ -1673,8 +1688,8 @@ function check_uboot_type {
 		USE_UENV=1
 		is_omap
 
-		 BETA_KERNEL=1
-		 SERIAL_MODE=1
+		BETA_KERNEL=1
+		SERIAL_MODE=1
 		;;
 	mx53loco)
 		SYSTEM="mx53loco"
