d117172b7e78a51ddb4da4a3411cbc27545f9331
Robert Nelson <robertcnelson@gmail.com>
2012-03-13 19:33:07 -0500

    drop all non-uEnv.txt bootscript devices
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -20,7 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-#Notes: need to check for: parted, fdisk, wget, mkfs.*, mkimage, md5sum
+#REQUIREMENTS:
+#uEnv.txt bootscript support
 
 unset MMC
 unset FIRMWARE
@@ -1654,19 +1655,6 @@ function check_uboot_type {
 
 		KMS_VIDEOB="video=HDMI-A-1"
 		BETA_KERNEL=1
-		;;
-
-	touchbook)
-		SYSTEM="touchbook"
-		DO_UBOOT=1
-		BOOTLOADER="TOUCHBOOK"
-		SERIAL="ttyO2"
-		is_omap
-		VIDEO_TIMING="1024x600MR-16@60"
-		KMS_VIDEO_RESOLUTION="1024x600"
-
-		BETA_KERNEL=1
-		SERIAL_MODE=1
 		;;
 	crane)
 		SYSTEM="crane"
