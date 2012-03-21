dadf0252aa1e44a1504f8474979766b16fe04591
Robert Nelson <robertcnelson@gmail.com>
2012-03-21 15:32:53 -0500

    board: add mx53evk target, only initial boot tested
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -598,7 +598,7 @@ function boot_uenv_txt_template {
 
 		__EOF__
 		;;
-	igepv2|crane|panda|panda_es|mx53loco)
+	igepv2|crane|panda|panda_es|mx51evk|mx53loco)
 		cat >> ${TEMPDIR}/bootscripts/netinstall.cmd <<-__EOF__
 			deviceargs=setenv device_args
 			loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; bootm \${address_uimage} \${address_uinitrd}
@@ -1447,16 +1447,13 @@ function is_omap {
 	unset KMS_VIDEOB
 }
 
-function is_imx53 {
- IS_IMX=1
- UIMAGE_ADDR="0x70800000"
- UINITRD_ADDR="0x72100000"
- SERIAL_CONSOLE="${SERIAL},115200"
- ZRELADD="0x70008000"
- SUBARCH="imx"
- VIDEO_CONSOLE="console=tty0"
- VIDEO_FB="mxcdi1fb"
- VIDEO_TIMING="RGB24,1280x720M@60"
+function is_imx {
+	IS_IMX=1
+	SERIAL_CONSOLE="${SERIAL},115200"
+	SUBARCH="imx"
+	VIDEO_CONSOLE="console=tty0"
+	VIDEO_FB="mxcdi1fb"
+	VIDEO_TIMING="RGB24,1280x720M@60"
 }
 
 function check_uboot_type {
@@ -1577,13 +1574,29 @@ function check_uboot_type {
 		BETA_KERNEL=1
 		SERIAL_MODE=1
 		;;
+	mx51evk)
+		SYSTEM="mx51evk"
+		DO_UBOOT=1
+		DD_UBOOT=1
+		BOOTLOADER="MX51EVK"
+		SERIAL="ttymxc0"
+		is_imx
+		ZRELADD="0x90008000"
+		UIMAGE_ADDR="0x90800000"
+		UINITRD_ADDR="0x92100000"
+		BETA_KERNEL=1
+		SERIAL_MODE=1
+		;;
 	mx53loco)
 		SYSTEM="mx53loco"
 		DO_UBOOT=1
 		DD_UBOOT=1
 		BOOTLOADER="MX53LOCO"
 		SERIAL="ttymxc0"
-		is_imx53
+		is_imx
+		ZRELADD="0x70008000"
+		UIMAGE_ADDR="0x70800000"
+		UINITRD_ADDR="0x72100000"
 		;;
 	*)
 		IN_VALID_UBOOT=1
