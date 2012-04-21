844ff7391864cf9fd2d25f3edd6a9f3c571bfef0
Robert Nelson <robertcnelson@gmail.com>
2012-04-21 07:34:16 -0500

    card script: check for software, before trying to figure out what version we have
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -111,29 +111,10 @@ function is_valid_addon {
 }
 
 function check_root {
-if [[ $UID -ne 0 ]]; then
- echo "$0 must be run as sudo user or root"
- exit
-fi
-}
-
-function find_issue {
-
-check_root
-
-#Software Qwerks
-
-#Check for gnu-fdisk
-#FIXME: GNU Fdisk seems to halt at "Using /dev/xx" when trying to script it..
-if fdisk -v | grep "GNU Fdisk" >/dev/null ; then
- echo "Sorry, this script currently doesn't work with GNU Fdisk"
- exit
-fi
-
-unset PARTED_ALIGN
-if parted -v | grep parted | grep 2.[1-3] >/dev/null ; then
- PARTED_ALIGN="--align cylinder"
-fi
+	if [[ $UID -ne 0 ]]; then
+		echo "$0 must be run as sudo user or root"
+		exit
+	fi
 }
 
 function check_for_command {
@@ -165,6 +146,18 @@ function detect_software {
 		echo "Gentoo: emerge u-boot-tools wget dosfstools parted dpkg"
 		echo ""
 		exit
+	fi
+
+	#Check for gnu-fdisk
+	#FIXME: GNU Fdisk seems to halt at "Using /dev/xx" when trying to script it..
+	if fdisk -v | grep "GNU Fdisk" >/dev/null ; then
+		echo "Sorry, this script currently doesn't work with GNU Fdisk"
+		exit
+	fi
+
+	unset PARTED_ALIGN
+	if parted -v | grep parted | grep 2.[1-3] >/dev/null ; then
+		PARTED_ALIGN="--align cylinder"
 	fi
 }
 
@@ -1730,7 +1723,7 @@ while [ ! -z "$1" ]; do
             then
 	        PARTITION_PREFIX="p"
             fi
-            find_issue
+            check_root
             check_mmc
             ;;
         --uboot)
@@ -1802,7 +1795,7 @@ fi
  echo "Script Version git: ${GIT_VERSION}"
  echo "-----------------------------"
 
- find_issue
+ check_root
  detect_software
  dl_bootloader
  dl_kernel_image
