d5e6f100ce721902c631c9820ad8162b3d2baabc
Robert Nelson <robertcnelson@gmail.com>
2012-06-26 14:16:18 -0500

    dont build nouveau
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -1284,7 +1284,7 @@ script_readme
 		git branch libdrm-build -D || true
 		git checkout 2.4.35 -b libdrm-build
 
-		./autogen.sh --prefix=/usr --libdir=/usr/lib/arm-linux-\${gnu} --disable-libkms --disable-intel --disable-radeon --enable-omap-experimental-api
+		./autogen.sh --prefix=/usr --libdir=/usr/lib/arm-linux-\${gnu} --disable-libkms --disable-intel --disable-radeon --disable-nouveau --enable-omap-experimental-api
 
 		make
 		sudo make install
