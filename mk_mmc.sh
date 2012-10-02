18fdfc8527c8354b4e7a6c8b0d50e8963b1f985d
Robert Nelson <robertcnelson@gmail.com>
2012-10-02 10:06:39 -0500

    distro: quantal netinstall update
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -76,10 +76,10 @@ PRECISE_ARMEL_MD5SUM="8e1f3d4a0df6bcf816f516e2226ba7f3"
 PRECISE_ARMHF_NETIMAGE="20101020ubuntu136"
 PRECISE_ARMHF_MD5SUM="2b8a00ada904f3b2b72f3d92ccbaa830"
 
-#22-Sep-2012
+#02-Oct-2012
 #http://ports.ubuntu.com/dists/quantal/main/installer-armhf/
-QUANTAL_ARMHF_NETIMAGE="20101020ubuntu176"
-QUANTAL_ARMHF_MD5SUM="f4a990b8eb69500d35db1d4ea7c7b334"
+QUANTAL_ARMHF_NETIMAGE="20101020ubuntu179"
+QUANTAL_ARMHF_MD5SUM="f2c99b949d3d23848e06e0b0ec17be89"
 
 #03-May-2012: 6.0.4+b1
 #http://ftp.us.debian.org/debian/dists/squeeze/main/installer-armel/
